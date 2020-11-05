529908d49aac4fd926a019050558510e51909499
Christian Bunk <christian.bunk@sensorberg.com>
2020-11-05 13:17:53 +0100

    adjsut alias.zsh

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -1,3 +1,4 @@
 alias push='git push origin'
 alias pushForce='git push --force origin'
 alias pullForce="$HOME/dev/private/toolbox/bash/forcePullBranch.sh"
+alias gclean="git checkout . && git reset HEAD"

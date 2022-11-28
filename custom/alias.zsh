bf9b01552d1ce780afcc23e5dbaae4c2eb7b490c
Christian Bunk <christian.bunk@nordsec.com>
2022-11-28 09:45:06 +0100

    save

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -1,4 +1,7 @@
 alias push='git push origin'
 alias pushForce='git push --force origin'
-alias pullForce="$HOME/dev/private/toolbox/bash/forcePullBranch.sh"
+alias pullForce="git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)"
 alias gclean="git checkout . && git reset HEAD"
+
+# reloading the zsh configuration
+alias reload=". ~/.zshrc"
